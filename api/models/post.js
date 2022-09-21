59cc0fefaec91ea050228e0c63f9be9522fcb777
nickwlong <nickwlong@gmail.com>
2022-09-21 11:37:00 +0100

    Added date posted to the post model

--- api/models/post.js
+++ api/models/post.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-  message: String
+  message: String,
+  datePosted: Date
 });
-
+// Update the route? That works with postman
+// Create some tests to check this works for the model file and controller
+// Integrate with the frontend? Date.now => 
 const Post = mongoose.model("Post", PostSchema);
 
+Post.insertMany([
+  {message: 'Test post one', datePosted: "2017-05-18T16:00:00Z"},
+  {message: 'Test post two', datePosted: "2018-05-18T16:00:00Z"},
+  {message: 'Test post three', datePosted: "2016-05-18T16:00:00Z"}
+]); 
+ 
 module.exports = Post;
--- api/spec/models/post.spec.js
+++ api/spec/models/post.spec.js
@@ -10,9 +10,12 @@ describe("Post model", () => {
     });
   });
 
-  it("has a message", () => {
-    var post = new Post({ message: "some message" });
+  it("has a message and date", () => {
+    var post = new Post({ message: "some message", datePosted: "2017-05-18T16:00:00Z" });
+    fakeDate = new Date("2017-05-18T16:00:00.000Z")
+    wrongFakeDate = new Date("2018-05-18T16:00:00.000Z")
     expect(post.message).toEqual("some message");
+    expect(post.datePosted).toMatchObject(fakeDate)
   });
 
   it("can list all posts", (done) => {
