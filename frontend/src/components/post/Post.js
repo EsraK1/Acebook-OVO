1c83d165ff70f137bf44e3f1ad9bb36cabef0d04
nickwlong <nickwlong@gmail.com>
2022-09-22 12:29:59 +0100

    Merged styling

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 const Post = ({post}) => {
+
+  const date = new Date(post.datePosted).toLocaleDateString("en-uk", {
+    hour: "2-digit",
+    minute: "2-digit",
+    year: "numeric",
+    month: "short",
+    weekday: "long",
+    day: "numeric",
+    }); 
+
   return(
-      <article data-cy="post" key={ post._id }>
-        <h2>{ post.datePosted }</h2>
-        <p>{ post.message }</p>
+      <article data-cy="post" className='post' key={ post._id }>
+        <h2 className="post-date">{ date }</h2>
+        <p >{ post.message }</p>
       </article>
   )
 }
