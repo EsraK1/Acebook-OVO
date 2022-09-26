49795106ddb817b9d14f605034467d4874bd624e
EsraK1 <esrakaya165@outlook.com>
2022-09-26 16:41:05 +0100

    posts like feature

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,9 +1,8 @@
-import React, {  } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 // changed the input of just 'post' to 'props' which contains all the properties of the Post element in Feed.js
 const Post = ( props ) => {
-
   const date = new Date(props.post.datePosted).toLocaleDateString("en-uk", {
     hour: "2-digit",
     minute: "2-digit",
@@ -30,12 +29,17 @@ const Post = ( props ) => {
   }
 
   const likeBtn = () => {
-    if (props.post.likes.includes(props.userId)) {return (<button style={{float: 'right', border: 'none', 'background-color': 'transparent'}} >{props.post.likes.length} likes: You have liked this post</button>)} else {
-      return(<button onClick={likeBtnSubmit} style={{float: 'right'}}>{props.post.likes.length} likes: Click here to like this post</button>)
+    if (props.post.likes.includes(props.userId)) {return (<button id={'likeButton'} style={{float: 'right', border: 'none', 'background-color': 'transparent'}} >{props.post.likes.length} likes: You have liked this post</button>)} else {
+        return(<button id={'likeButton'} onClick={() =>{likeBtnSubmit() ; refreshPage()}} style={{float: 'right'}}>{props.post.likes.length} likes: Click here to like this post</button>)
     }
   }
 
-  const likeBtnSubmit = async () => {
+
+  function refreshPage(){ 
+    window.location.reload(); 
+  }
+
+  const likeBtnSubmit = async () => { 
       fetch( '/posts', {
         method: 'put',
         headers: {
@@ -44,6 +48,7 @@ const Post = ( props ) => {
         },
         body: JSON.stringify({ _id: props.post._id, userId: props.userId })
       })
+    
         .then(response => {
           if(response.status === 200) {
             console.log(response)
@@ -51,8 +56,7 @@ const Post = ( props ) => {
             alert('oops something is wrong')
           }
         })
-    }
-  
+    
 
   return(
       <article data-cy="post" className='post' key={ props.post._id }>
@@ -64,5 +68,11 @@ const Post = ( props ) => {
       </article>
   )
 }
+}
+
+
+// Try adding an unlike feature so the same button can be used to like and unliked by a user
+// We need to think about how the user id will be removed from the array for likes when 'unliking'
+// maybe try reengineer the '#likeBtnSubmit' above to make it unlike posts
 
 export default Post;
