f41574d4bc376cbee83dbd9c0fe22a51d90160fd
EsraK1 <esrakaya165@outlook.com>
2022-09-27 11:04:57 +0100

    solved merge conflicts

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -38,11 +38,6 @@ const Post = (props) => {
     }
   }
 
-  const deleteBtnAppears = (() => {if (props.post.postauthor._id===props.user_id) {return <button onClick= {deleteFunction} id={"deleteBtn"} title={"Delete post"}>Delete Post</button>}})
-
-  function refreshPage(){
-    window.location.reload();
-    }
 
   return(
       <article data-cy="post" className='post' key={ props.post._id }>
